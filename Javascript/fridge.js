9d2ede0bdf2baa4cb8fb7e337ef8d3758830c9d0
Khashgerel <77399639+Khashgerel@users.noreply.github.com>
2024-12-06 13:01:14 +0800

    fridge barag l bolson bas mongolruu orchuulsan

---
+++ Javascript/fridge.js
@@ -0,0 +1,130 @@
+let recipesData = [];
+let ingredientsData = [];
+let activeIngredients = []; 
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetch('/json/recipe.json')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Network response was not ok: ${response.statusText}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            if (data && data.recipes) {
+                recipesData = data.recipes;
+            } else {
+                console.error('Data format error: No "recipes" array in JSON');
+            }
+        })
+        .catch(error => console.error('Error fetching recipes:', error));
+
+    fetch('/json/ingredients.json')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Network response was not ok: ${response.statusText}`);
+            }
+            return response.json();
+        })
+        .then(dt => {
+            if (dt) {
+                ingredientsData = dt;
+                setupIngredients();
+                searchIngredients();
+                selectIngredient();
+            } else {
+                console.error('Data format error: No valid data in JSON');
+            }
+        })
+        .catch(error => console.error('Error fetching ingredients:', error));
+});
+
+function setupIngredients() {
+    const orts = document.querySelector('.orts-list');
+    orts.innerHTML = '';
+
+    ingredientsData.forEach(ingredient => {
+        const listItem = document.createElement('li');
+        listItem.className = 'ingredient';
+        listItem.textContent = ingredient;
+        orts.appendChild(listItem);
+    });
+    
+}
+
+function searchIngredients() {
+    const searchInput = document.getElementById('orts-search');
+    const ingredientsItems = document.querySelectorAll('.orts-list li');
+
+    searchInput.addEventListener('input', () => {
+        const searchTerm = searchInput.value.toLowerCase();
+
+        ingredientsItems.forEach(item => {
+            const ingredient = item.textContent.toLowerCase();
+            if (ingredient.includes(searchTerm)) {
+                item.style.display = '';
+            } else {
+                item.style.display = 'none';
+            }
+        });
+    });
+}
+
+function selectIngredient() {
+    const ingredientsItems = document.querySelectorAll('.orts-list li');
+
+    ingredientsItems.forEach(ingredientsItem => {
+        ingredientsItem.addEventListener('click', () => {
+            const ingredient = ingredientsItem.textContent;
+
+            if (activeIngredients.includes(ingredient)) {
+                activeIngredients = activeIngredients.filter(item => item !== ingredient);
+                ingredientsItem.classList.remove('active');
+            } else {
+                activeIngredients.push(ingredient);
+                ingredientsItem.classList.add('active');
+            }
+
+            filterFood();
+        });
+    });
+}
+
+function filterFood() {
+    const container = document.querySelector('.container');
+
+    container.innerHTML = '';
+
+    if (activeIngredients.length === 0) {
+        recipesData.forEach(recipe => renderRecipe(recipe, container));
+        return;
+    }
+
+    const filteredRecipes = recipesData.filter(recipe =>
+        activeIngredients.every(activeIngredient =>
+            recipe.ingredients.some(recipeIngredient =>
+                recipeIngredient.toLowerCase().includes(activeIngredient.toLowerCase())
+            )
+        )
+    );
+
+    if (filteredRecipes.length > 0) {
+        filteredRecipes.forEach(recipe => renderRecipe(recipe, container));
+    } else {
+        const noResultMessage = document.createElement('p');
+        noResultMessage.textContent = 'No recipes match the selected ingredients.';
+        container.appendChild(noResultMessage);
+    }
+}
+
+function renderRecipe(recipe, container) {
+    const foodCard = document.createElement('section');
+    foodCard.className = 'food-card';
+    foodCard.innerHTML = `
+        <img src='${recipe.image}' alt='${recipe.name}'>
+        <a href='/htmls/hool_detail.html?id=${recipe.id}'>
+        <h3>${recipe.name}</h3>
+        </a>
+    `;
+    container.appendChild(foodCard);
+}
