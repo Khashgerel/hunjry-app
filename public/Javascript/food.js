c50aa2563483993d692c2c911eaa3f578d8c5bc0
Aiym005 <158027287+Aiym005@users.noreply.github.com>
2025-01-07 14:56:06 +0800

    .

---
+++ public/Javascript/food.js
@@ -0,0 +1,25 @@
+class Food extends HTMLElement {
+    constructor() {
+        super();
+        //implementation
+    }
+
+    connectedCallback() {
+        //implementation
+    }
+
+    disconnectedCallback() {
+        //implementation
+    }
+
+    attributeChangedCallback(name, oldVal, newVal) {
+        //implementation
+    }
+
+    adoptedCallback() {
+        //implementation
+    }
+
+}
+
+window.customElements.define('food', Food);
